27c70e384bb0d71f312e980f89136f86d0ea2f37
jlduan <jlduan@users.noreply.github.com>
2020-10-08 01:48:22 -0500

    Add .bumpversion.cfg

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='fba',
-    version='0.0.5dev',
+    version='0.0.5',
     author='JD',
     description='Tools for feature barcoding analyses',
     long_description=long_description,
