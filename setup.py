0522f4e3f42cd56467ef3fd8c94f6aee744c8477
jlduan <jlduan@users.noreply.github.com>
2021-02-10 15:57:14 -0600

    Bump version: 0.0.6 → 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='fba',
-    version='0.0.6',
+    version='0.0.7',
     author='JD',
     description='Tools for feature barcoding analyses',
     long_description=long_description,
