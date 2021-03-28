b25fcd9a787f2a0e8d158673ec8646519d5cf0ec
jlduan <jlduan@users.noreply.github.com>
2021-03-27 20:08:53 -0500

    Update fba.yml

--- tests/test_levenshtein.py
+++ tests/test_levenshtein.py
@@ -1,5 +1,6 @@
 # test_levenshtein.py
 
+import pytest
 from fba.levenshtein import (
     indexkeys,
     bytes2set,
@@ -13,9 +14,18 @@ from fba.levenshtein import (
 )
 
 
-def test_indexkeys():
-    words = {'ACG', 'ACGT', 'ACT', 'AGT', 'CGT'}
-    assert indexkeys(word='ACGT', max_dist=1) == words
+test_data = [
+    ('ACGT', 0, {'ACGT'}),
+    ('ACGT', 1, {'ACG', 'ACGT', 'ACT', 'AGT', 'CGT'}),
+    ('abc', 2, {'a', 'ab', 'abc', 'ac', 'b', 'bc', 'c'}),
+    ('ACGTACGT', 0, {'ACGTACGT'}),
+    ('A', 2, {'', 'A'})
+]
+
+
+@pytest.mark.parametrize('word, max_dist, z', test_data)
+def test_indexkeys(word, max_dist, z):
+    assert indexkeys(word=word, max_dist=max_dist) == z
 
 
 def test_bytes2set():
@@ -26,8 +36,28 @@ def test_set2bytes():
     assert set2bytes({u'a', u'b', u'c'}) == b'a\x00b\x00c'
 
 
-def test_create_index():
-    a = create_index(barcodes=['ACGT'], num_mismatches=1)
-    b = {'CGT': 'ACGT', 'ACT': 'ACGT',
-         'ACGT': 'ACGT', 'AGT': 'ACGT', 'ACG': 'ACGT'}
-    assert a == b
+test_data = [
+    (['AC'], 0, {'AC': 'AC'}),
+    (['AC'], 1, {'A': 'AC', 'AC': 'AC', 'C': 'AC'}),
+    (['AC', 'AG'], 0, {'AC': 'AC', 'AG': 'AG'}),
+    (['A'], 3, {'': 'A', 'A': 'A'}),
+    (['123'], 1, {'13': '123', '23': '123', '123': '123', '12': '123'})
+]
+
+
+@pytest.mark.parametrize('barcodes, num_mismatches, z', test_data)
+def test_create_index(barcodes, num_mismatches, z):
+    assert create_index(barcodes=barcodes, num_mismatches=num_mismatches) == z
+
+
+test_data = [
+    ('AC', {'AC': 'AC'}, 0, {0: ['AC']})
+]
+
+
+@pytest.mark.parametrize('seq, barcode_index, num_mismatches, z', test_data)
+def test_query_index(seq, barcode_index, num_mismatches, z):
+    assert query_index(
+        seq=seq,
+        barcode_index=barcode_index,
+        num_mismatches=num_mismatches) == z
