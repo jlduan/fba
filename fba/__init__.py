4673865a6f146c183762f6151e405daa8ac776da
jlduan <jlduan@users.noreply.github.com>
2022-03-12 02:26:36 -0600

    Update

--- fba/__init__.py
+++ fba/__init__.py
@@ -1,18 +1,3 @@
 # __init__.py
 
-import subprocess
-from pathlib import Path
-
 __version__ = '0.0.12'
-
-package_root = Path(__file__).resolve().parent.parent
-if (package_root / '.git').exists():
-
-    cmd = 'git describe --tags --always --dirty'.split()
-    try:
-        __version__ = subprocess.check_output(
-            cmd, cwd=package_root).decode().strip()
-
-    except subprocess.CalledProcessError:
-        print('Unable to get version number from git tags')
-        exit(1)
