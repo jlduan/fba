ac7d26e8b2ee647e08c18fa3e8cf2402a5df3ce9
jlduan <jlduan@users.noreply.github.com>
2022-03-13 12:22:22 -0500

    Update

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.12-11-gc9843b3-dirty',
+    VERSION: '0.0.12-12-gd251184-dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
