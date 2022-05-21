e1a84b50014027e3789a526f05dfa621fb2b65b0
jlduan <jlduan@users.noreply.github.com>
2022-05-21 16:00:39 -0500

    Update tutorial.rst

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.12-19-gd31b471-dirty',
+    VERSION: '0.0.12-20-g7636533-dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
